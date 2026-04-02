de6d869c8ea93a8d7b67193cc2b5dfb6fce087b1
Kadir <kadir@kadirgun.com>
2026-04-02 14:01:13 +0300

    feat: add bravo schema and validator using vine

---
+++ validators/bravo.ts
@@ -0,0 +1,15 @@
+import vine from '@vinejs/vine'
+
+export const bravoSchema = {
+  sort: vine
+    .object({
+      field: vine.string(),
+      order: vine.enum(['asc', 'desc'] as const),
+    })
+    .optional(),
+  limit: vine.number().positive().max(100).optional(),
+  page: vine.number().positive().optional(),
+  include: vine.array(vine.string()).optional(),
+}
+
+export const bravoValidator = vine.create(bravoSchema)
